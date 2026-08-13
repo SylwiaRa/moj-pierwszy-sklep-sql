bed00d7b2dc0fd4a9ace3ebae1b5f03b82c27cd8
SylwiaRa <38177850+SylwiaRa@users.noreply.github.com>
2026-08-13 21:25:15 +0200

    Update zapytania.sql

--- zapytania.sql
+++ zapytania.sql
@@ -13,3 +13,9 @@ ORDER BY razem_wydano DESC;
 SELECT produkty.nazwa, produkty.stan_magazynowy
 FROM produkty
 WHERE produkty.stan_magazynowy < 10;
+
+UPDATE produkty
+SET stan_magazynowy = 25
+WHERE id = 3;
+
+SELECT * FROM produkty;
