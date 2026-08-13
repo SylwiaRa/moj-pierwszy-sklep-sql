f5d6b010235c8d2858d467ccc89b1753970f9274
SylwiaRa <38177850+SylwiaRa@users.noreply.github.com>
2026-08-13 20:41:54 +0200

    Update zapytania.sql

--- zapytania.sql
+++ zapytania.sql
@@ -3,3 +3,9 @@ FROM klienci
 INNER JOIN zamowienia on klienci.id = zamowienia.klient_id
 INNER JOIN produkty on zamowienia.produkt_id = produkty.id;
 
+SELECT klienci.imie, SUM(zamowienia.ilosc * produkty.cena) as razem_wydano
+FROM klienci
+INNER JOIN zamowienia on klienci.id = zamowienia.klient_id
+INNER JOIN produkty on zamowienia.produkt_id = produkty.id
+GROUP BY klienci.imie
+ORDER BY razem_wydano DESC;
