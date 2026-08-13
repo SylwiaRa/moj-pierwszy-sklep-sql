34b5e93b9aaa163430cfeeae21a4b0d1226ba242
SylwiaRa <38177850+SylwiaRa@users.noreply.github.com>
2026-08-13 21:00:55 +0200

    Update zapytania.sql

--- zapytania.sql
+++ zapytania.sql
@@ -9,3 +9,7 @@ INNER JOIN zamowienia on klienci.id = zamowienia.klient_id
 INNER JOIN produkty on zamowienia.produkt_id = produkty.id
 GROUP BY klienci.imie
 ORDER BY razem_wydano DESC;
+
+SELECT produkty.nazwa, produkty.stan_magazynowy
+FROM produkty
+WHERE produkty.stan_magazynowy < 10;
