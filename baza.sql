b5b00761992075875e37b2325f51ba399d76c64a
SylwiaRa <38177850+SylwiaRa@users.noreply.github.com>
2026-08-15 20:03:11 +0200

    Update baza.sql

--- baza.sql
+++ baza.sql
@@ -37,35 +37,6 @@ CREATE TABLE zamowienia (
 CREATE TABLE recenzje (
     id SERIAL PRIMARY KEY,
     produkt_id INT REFERENCES produkty(id) ON DELETE CASCADE,
-    ocena INT NOT NULL,
+    ocena INT NOT NULL CHECK (ocena >= 1 AND ocena <= 5),
     komentarz VARCHAR(200)
 );
-
--- ==========================================
--- 3. ZASILANIE BAZY DANYCH (DANE TESTOWE)
--- ==========================================
-
--- Wrzucamy pełny asortyment produktów
-INSERT INTO produkty (nazwa, cena, stan_magazynowy) VALUES
-('Laptop ASUS', 4500.00, 10),
-('Myszka Logitech', 150.00, 40),
-('Fotel Diablo', 899.00, 5),
-('Słuchawki Razer', 350.00, 15);
-
--- Wrzucamy konta klientów
-INSERT INTO klienci (imie, email) VALUES
-('Jan', 'jan@email.com'),
-('Anna', 'anna@email.com'),
-('Marek', 'marek@email.com');
-
--- Wrzucamy pierwsze zakupy
-INSERT INTO zamowienia (klient_id, produkt_id, ilosc) VALUES
-(1, 2, 1),
-(2, 1, 1),
-(2, 3, 1);
-
--- Wrzucamy oceny produktów
-INSERT INTO recenzje (produkt_id, ocena, komentarz) VALUES
-(1, 5, 'Super laptop, bardzo szybki!'),
-(1, 4, 'Działa świetnie, ale głośny pod obciążeniem'),
-(3, 5, 'Wygodny fotel, plecy już nie bolą');
